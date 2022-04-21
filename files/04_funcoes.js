efe6235f7aaa7487a6948d0c2b613e0722b2dff2
Antônio Rocha <antoniojunior159@gmail.com>
2022-04-21 11:30:22 -0300

    Update 04_funcoes.js

--- files/04_funcoes.js
+++ files/04_funcoes.js
@@ -5,9 +5,28 @@
 }*/
 
 
-function sayHello () {
-	alert("Hello")
+//Função que diz Hello
+function sayHello() {
+    console.log("Hello, programa funcionando!!!")
 }
-	sayHello()
+sayHello() // Chamada da função
 
-//RETURN
+
+// Ex: Função que soma dois números
+
+function somaNumeros(num1, num2) {
+    console.log(num1 + num2)
+}
+somaNumeros(5,4) //Chamada da função passando os valores
+
+//Ex: Função que verifica se o número é par ou ímpar
+
+function parImp (numero) {
+    if (numero%2 == 0) {
+        console.log("O número é par")
+    } else {
+        console.log("O numero é ímpar")
+    }
+}
+parImp(8) // Call
+
